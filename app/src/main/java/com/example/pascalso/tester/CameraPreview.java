271f82a957e049f4488dbd04c20f72e3fcc1664c
08351ty <pascal.so@gmail.com>
2015-06-25 16:22:06 +0800

    Save #1
    1.) set up the camera preview
    2.) Insert image buttons on bottom FrameLayout

---
+++ app/src/main/java/com/example/pascalso/tester/CameraPreview.java
@@ -0,0 +1,64 @@
+package com.example.pascalso.tester;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by owner on 6/25/15.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera){
+        super(context);
+        mCamera = camera;
+        mCamera.setDisplayOrientation(90);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
+
+    }
+    public void surfaceCreated(SurfaceHolder surfaceHolder){
+        try{
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
+        }
+        catch (IOException e){
+            Log.d("ERROR", "Camera error on surfaceCreated " + e.getMessage());
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int a, int b, int c){
+        if(mHolder.getSurface() == null)
+            return;
+
+
+        try{
+            mCamera.stopPreview();
+        }
+        catch (Exception e){
+
+        }
+
+        try{
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        }
+        catch (IOException e){
+            Log.d("ERROR", "Camera error on surfaceChanged" + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder){
+        mCamera.stopPreview();
+        mCamera.release();
+    }
+}
---
+++ app/src/main/java/com/example/pascalso/tester/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.pascalso.tester;
+
+import android.hardware.Camera;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.app.Activity;
+
+
+public class MainActivity extends Activity{
+    private Camera mCamera;
+    private CameraPreview mCameraPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+            mCamera = Camera.open();
+        }
+        catch(Exception e){
+            Log.d("ERROR", "Failed to get camera " + e.getMessage());
+        }
+
+        if(mCamera != null){
+            mCameraPreview = new CameraPreview(this, mCamera);
+            FrameLayout camera_preview = (FrameLayout) findViewById(R.id.camera_preview);
+            camera_preview.addView(mCameraPreview);
+        }
+    }
+    /**
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+}
